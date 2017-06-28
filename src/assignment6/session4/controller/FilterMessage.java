d6eddf9009563f6493a3670aa2cba79b8898a73b
vikasvks90 <vikas.varshney90@gmail.com>
2017-06-28 17:00:52 +0530

    First Commit

---
+++ src/assignment6/session4/controller/FilterMessage.java
@@ -0,0 +1,51 @@
+package assignment6.session4.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class FilterMessage
+ */
+@WebFilter("/ServletMessage")
+public class FilterMessage implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public FilterMessage() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		chain.doFilter(request, response);
+		out.println("<br>Message from Filter!!!<br>");
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
